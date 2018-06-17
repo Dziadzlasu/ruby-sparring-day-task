f1ca2563f858b8951ce10990f6ada5e3b4007212
Dariusz <pixelexcel@gmail.com>
2018-06-17 14:38:21 +0200

    add methods greeting & goodbye & refactor

--- services/zoo.rb
+++ services/zoo.rb
@@ -8,27 +8,8 @@ require_relative '../services/lemur'
 class Zoo
   def inventory
     # binding.pry
-    if @count.nil?
-      'Oh no! There are no animals in our ZOO!'
-    elsif @count == 1
-      "There is only one animal in our ZOO! #{list_animals}. "\
-      'Hope you will enjoy your visit!'
-    elsif @count == 2 && @animal.all? { |param| param.is_a?(Gorilla) }
-      "For now there are only #{@count} gorillas here! #{list_animals}. "\
-      'Hope you will enjoy your visit!'
-    elsif @count == 2
-      "For now there are only #{@count} animals here! #{list_animals}. "\
-      'Hope you will enjoy your visit!'
-    elsif @count == 4
-      "For now there are only #{@count} animals here! #{list_animals}. "\
-      'Hope you will enjoy your visit!'
-    elsif @count > 4 && @count < 10
-      "There are #{@count} animals in our ZOO! #{list_animals}. "\
-      'Hope you will enjoy your visit!'
-    elsif @count == 10
-      "Yayy! We have #{@count} residents now! #{list_animals}. "\
-      'You need to visit us as soon as possible!'
-    end
+    return greeting if @count.nil?
+    "#{greeting}#{list_animals}. #{goodbye}"
   end
 
   def add_animals(animal)
@@ -39,5 +20,26 @@ class Zoo
   def list_animals
     return @animal.map(&:info).join(' and ') if @count == 2
     @animal.map(&:info).join(', ')
+  end
+
+  def greeting
+    if @count.nil?
+      'Oh no! There are no animals in our ZOO!'
+    elsif @count == 1
+      'There is only one animal in our ZOO! '
+    elsif @count == 2 && @animal.all? { |param| param.is_a?(Gorilla) }
+      "For now there are only #{@count} gorillas here! "
+    elsif @count > 1 && @count < 5
+      "For now there are only #{@count} animals here! "
+    elsif @count > 4 && @count < 10
+      "There are #{@count} animals in our ZOO! "
+    elsif @count == 10
+      "Yayy! We have #{@count} residents now! "
+    end
+  end
+
+  def goodbye
+    return 'Hope you will enjoy your visit!' if !@count.nil? && @count > 0 && @count < 10
+    'You need to visit us as soon as possible!'
   end
 end
