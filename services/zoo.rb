1144ddea78c85fe27809d465a4c94d9086a34dac
Dariusz <pixelexcel@gmail.com>
2018-06-17 18:03:46 +0200

    refactor greeting & delete obsolete conditional from goodbye

--- services/zoo.rb
+++ services/zoo.rb
@@ -22,23 +22,20 @@ class Zoo
   end
 
   def greeting
-    if @count.zero?
-      'Oh no! There are no animals in our ZOO!'
-    elsif @count == 1
-      'There is only one animal in our ZOO! '
-    elsif @count > 1 && @animal.map(&:class).uniq.length == 1
+    case
+    when @count.zero? then 'Oh no! There are no animals in our ZOO!'
+    when @count == 1 then 'There is only one animal in our ZOO! '
+    when @count > 1 && @animal.map(&:class).uniq.length == 1
       "For now there are only #{@count} #{@animal.first.class.name.downcase}s here! "
-    elsif @count > 1 && @count < 5
-      "For now there are only #{@count} animals here! "
-    elsif @count > 4 && @count < 10
-      "There are #{@count} animals in our ZOO! "
+    when @count > 1 && @count < 5 then "For now there are only #{@count} animals here! "
+    when @count > 4 && @count < 10 then "There are #{@count} animals in our ZOO! "
     else
       "Yayy! We have #{@count} residents now! "
     end
   end
 
   def goodbye
-    return 'Hope you will enjoy your visit!' if @count > 0 && @count < 10
+    return 'Hope you will enjoy your visit!' if @count < 10
     'You need to visit us as soon as possible!'
   end
 end
